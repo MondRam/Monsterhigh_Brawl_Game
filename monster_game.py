80d8c6076b0b3d12e97911483a25593731f74125
Sofia Campos <sfabilene@gmail.com>
2023-06-15 12:18:43 -0500

    feat: library

--- monster_game.py
+++ monster_game.py
@@ -1,1 +1,15 @@
 #Monsterhigh_Brawl_Game
+''' MONSTERHIGH BRAWL GAME 
+Montserrat Guadalupe Ramírez Esparza
+Sofia Abilene Campos Hernandez'''
+
+#Import the random library to choose random elements
+import random
+
+#Create a class to represent a type of monster
+class MonsterhighType:
+    def __init__(self, name, strengths=None, weaknesses=None):
+        self.name = name
+        self.strengths = [] if strengths is None else strengths
+        self.weaknesses = [] if weaknesses is None else weaknesses
+ 
