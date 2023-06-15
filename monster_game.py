17463e8c8123ea7497c925ef15b36f34036f5ee8
Sofia Campos <sfabilene@gmail.com>
2023-06-15 15:45:41 -0500

    feat:add attack

--- monster_game.py
+++ monster_game.py
@@ -12,4 +12,23 @@ class MonsterhighType:
         self.name = name
         self.strengths = [] if strengths is None else strengths
         self.weaknesses = [] if weaknesses is None else weaknesses
- 
+ #Check what type of monster it is strong against        
+    def strong_against (self, other):
+        return other.name in self.weaknesses
+        
+#Check what type of monster it is weak against        
+    def is_weak_against(self, other):
+        return other.name in self.strengths
+    
+#Represents a Monster with name, level, types and adds two types of attacks
+class Monsterhigh:
+    
+    def __init__(self, name, level, types):
+        self.name = name
+        self.level = level
+        self.types = types
+        self.max_health = level * 10
+        self.current_health = self.max_health
+        self.is_ko = False
+        self.general_attack = "None"
+        self.special_attack = "None"
